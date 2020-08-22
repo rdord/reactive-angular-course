40463e9149783cb1be00b94a79dc2aa6506dd500
Roland Đorđević <roland.djordjevic@ineor.si>
2020-08-22 15:24:31 +0200

    implement loader on the edit course dialog

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,25 +1,15 @@
-import {Component, OnInit} from '@angular/core';
-import {LoadingService} from './loading/loading.service';
+import { Component, OnInit } from '@angular/core';
+import { LoadingService } from './loading/loading.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
-  providers: [LoadingService]
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent implements  OnInit {
+export class AppComponent implements OnInit {
+  constructor() {}
 
-    constructor() {
+  ngOnInit() {}
 
-    }
-
-    ngOnInit() {
-
-
-    }
-
-  logout() {
-
-  }
-
+  logout() {}
 }
--- src/app/course-dialog/course-dialog.component.ts
+++ src/app/course-dialog/course-dialog.component.ts
@@ -1,56 +1,53 @@
-import {AfterViewInit, Component, ElementRef, Inject, OnInit, ViewChild, ViewEncapsulation} from '@angular/core';
-import { MAT_DIALOG_DATA, MatDialogRef } from "@angular/material/dialog";
-import {Course} from "../model/course";
-import {FormBuilder, Validators, FormGroup} from "@angular/forms";
+import { AfterViewInit, Component, ElementRef, Inject, OnInit, ViewChild, ViewEncapsulation } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { Course } from '../model/course';
+import { FormBuilder, Validators, FormGroup } from '@angular/forms';
 import * as moment from 'moment';
-import {catchError} from 'rxjs/operators';
-import {throwError} from 'rxjs';
-import {CoursesService} from '../services/courses.service';
-import {LoadingService} from '../loading/loading.service';
+import { catchError } from 'rxjs/operators';
+import { throwError } from 'rxjs';
+import { CoursesService } from '../services/courses.service';
+import { LoadingService } from '../loading/loading.service';
 
 @Component({
-    selector: 'course-dialog',
-    templateUrl: './course-dialog.component.html',
-    styleUrls: ['./course-dialog.component.css']
+  selector: 'course-dialog',
+  templateUrl: './course-dialog.component.html',
+  styleUrls: ['./course-dialog.component.css']
 })
 export class CourseDialogComponent implements AfterViewInit {
+  editCourseForm: FormGroup;
 
-    editCourseForm: FormGroup;
+  course: Course;
 
-    course: Course;
+  constructor(
+    private fb: FormBuilder,
+    private dialogRef: MatDialogRef<CourseDialogComponent>,
+    @Inject(MAT_DIALOG_DATA) courseData: Course,
+    private coursesService: CoursesService,
+    private loadingService: LoadingService
+  ) {
+    this.course = courseData;
 
-    constructor(
-        private fb: FormBuilder,
-        private dialogRef: MatDialogRef<CourseDialogComponent>,
-        @Inject(MAT_DIALOG_DATA) courseData: Course,
-        private coursesService: CoursesService,
-        private loadingService: LoadingService) {
+    this.editCourseForm = fb.group({
+      description: [this.course.description, Validators.required],
+      category: [this.course.category, Validators.required],
+      releasedAt: [moment(), Validators.required],
+      longDescription: [this.course.longDescription, Validators.required]
+    });
+  }
 
-        this.course = courseData;
+  ngAfterViewInit() {}
 
-        this.editCourseForm = fb.group({
-            description: [this.course.description, Validators.required],
-            category: [this.course.category, Validators.required],
-            releasedAt: [moment(), Validators.required],
-            longDescription: [this.course.longDescription, Validators.required]
-        });
+  save() {
+    const updatedCourse = this.editCourseForm.value;
 
-    }
+    const saveCourse$ = this.coursesService.updateCourse(`${this.course.id}`, updatedCourse);
 
-    ngAfterViewInit() {
+    this.loadingService.showLoaderUntilCompleted(saveCourse$).subscribe(res => {
+      this.dialogRef.close(res);
+    });
+  }
 
-    }
-
-    save() {
-      const updatedCourse = this.editCourseForm.value;
-      this.coursesService.updateCourse(`${this.course.id}`, updatedCourse).subscribe(
-        res => {
-          this.dialogRef.close(res);
-        });
-    }
-
-    close() {
-        this.dialogRef.close();
-    }
-
+  close() {
+    this.dialogRef.close();
+  }
 }
